57b5f9badb9666e77cbae60c73db746c27825ff2
Ramesh Appat <rameshappat@gmail.com>
2025-12-25 16:18:31 -0700

    feat: add RateRepository.java

---
+++ src/main/java/com/organization/rates/repository/RateRepository.java
@@ -0,0 +1,9 @@
+package com.organization.rates.repository;
+
+import com.organization.rates.model.Rate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RateRepository extends JpaRepository<Rate, Long> {
+}
